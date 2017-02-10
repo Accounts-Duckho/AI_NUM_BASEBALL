2583ea2bacca3635de6b9c3bad3c4e2aeb737f3c
atti98 <Jungdeokho@gmail.com>
2017-02-10 23:40:20 +0900

    add some enum and make inline func

--- game.h
+++ game.h
@@ -1,6 +1,7 @@
-void loadGame(void);
+void loadGame(int mode);
 void startGame(int* guess_answer);
 void checkGame(struct num *num, struct record *record, int count, int* round_strike, int* round_ball);
 void resultGame(int result_code);
-void evaluate(int* num, int result);
-enum {WIN=0, LOSE, YES=1, VERYHIGH, HIGH, NORMAL, LOW, SOLOW, VERYLOW, NO, FIRST=0, MIDDLE, LAST};
+__inline void evaluate(int* num, int result);
+void initialize(struct num *num, struct record *record, int *count, int* round_strike, int* round_ball);
+enum {WIN=0, LOSE, YES=1, VERYHIGH, HIGH, NORMAL, LOW, SOLOW, VERYLOW, NO, FIRST=0, MIDDLE, LAST, NEW=1, REPLAY};
